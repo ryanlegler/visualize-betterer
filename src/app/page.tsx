75904d1a46f483a099f708528f20686992c7e194
Ryan Legler <ryan.legler@gmail.com>
2023-12-03 14:58:18 -0800

    initial commit - basic project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Wrapper } from "@/components/Wrapper";
+
+// this app is so far doing nothing with RSC
+// wrapper is a component which handles all the drag n drop of the report json as well at the chart rendering.
+export default function Home() {
+    return <Wrapper />;
+}
